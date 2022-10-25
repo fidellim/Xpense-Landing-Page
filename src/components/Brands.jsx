ec0a307c8f5511b89907d7aecadce4e74f1df177
fidellim <lim.fidel@gmail.com>
2022-10-25 18:53:29 +0400

    design brands component

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { overview } from '../constants'
 
 const Brands = () => {
-    return <div>Brands</div>
+    // destructure overview
+    const { brands } = overview
+    return (
+        <section className="py-9">
+            <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6">
+                {/* brands */}
+                {brands.map((item, index) => {
+                    // destructure item
+                    const { image, delay } = item
+                    return (
+                        <div
+                            key={index}
+                            data-aos="fade-up"
+                            data-aos-delay={delay}
+                        >
+                            {/* brand img */}
+                            <img src={image} alt="" />
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
+    )
 }
 
 export default Brands
