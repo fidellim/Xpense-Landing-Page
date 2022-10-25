ecafa45caa88849ae06bbe18397c5bdf5b79c7e7
fidellim <lim.fidel@gmail.com>
2022-10-25 17:10:44 +0400

    initialize AOS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-function App() {
-    return (
-        <div className="flex justify-center items-center bg-slate-800 min-w-screen min-h-screen">
-            <header className="flex flex-col justify-center items-center text-white">
-                <img
-                    src={logo}
-                    className="w-[150px] h-[150px] animate-[spin_5s_linear_infinite]"
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    )
+const App = () => {
+    AOS.init({
+        duration: 1800,
+        offset: 100,
+    })
+    return <div>App</div>
 }
 
 export default App
